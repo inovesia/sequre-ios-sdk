c90130e5c5197ba91efdb16326ef5074adba0580
septiadi70 <septiadi70@gmail.com>
2024-08-21 22:42:19 +0700

    Fix: Scan camera view stretch when switching apps

--- SequreSDK/SequreScannerView.swift
+++ SequreSDK/SequreScannerView.swift
@@ -231,7 +231,7 @@ public struct SequreScannerView: View {
     
     func statusBarHeight() -> CGFloat {
         guard let window = UIApplication.shared.connectedScenes
-            .filter({$0.activationState == .foregroundActive})
+//            .filter({$0.activationState == .foregroundActive})
             .compactMap({$0 as? UIWindowScene})
             .first?.windows
             .filter({$0.isKeyWindow}).first else { return 0 }
